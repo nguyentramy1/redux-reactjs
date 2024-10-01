2592831be71c83749531c57b2de36a34f8861a37
nguyentramy1 <168984915+nguyentramy1@users.noreply.github.com>
2024-10-01 14:41:23 +0700

    Update table.tsx

--- src/Common/table/table.tsx
+++ src/Common/table/table.tsx
@@ -15,30 +15,53 @@ const Table: React.FC<TableProps> = ({ data, columnNames }) => {
 
   return (
     <div className="container">
-      <table>
-        <thead>
-          <tr>
-            <th>Số TT</th>
-            {headers.map((header, index) => (
-              <th key={index}>{columnNames[header].label || ""}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item, index) => (
-            <tr key={index}>
-              <td>{index + 1}</td>
-              {headers.map((key) => (
-                <td key={key}>
-                  {columnNames[key].render
-                    ? columnNames[key].render(item[key])
-                    : item[key]}
-                </td>
+      <div className="wrapper">
+        <table>
+          <thead>
+            <tr>
+              <th
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  color: "#001F2D",
+                  fontSize: "14px",
+                  // width: "80px",
+                }}
+              >
+                No
+              </th>
+              {headers.map((header, index) => (
+                <th key={index}>{columnNames[header].label || ""}</th>
               ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+
+          <tbody>
+            {data.map((item, index) => (
+              <tr key={index}>
+                <td
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    color: "#001F2D",
+                    fontSize: "14px",
+                    // width: "80px",
+                  }}
+                >
+                  {index + 1}
+                </td>
+                {headers.map((key) => (
+                  <td key={key}>
+                    {columnNames[key].render
+                      ? columnNames[key].render(item[key])
+                      : item[key]}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
